4d4cef228ee336fc0ce7968a6acb972aded8c7a0
MatvejBeltsov <matvej.beltsov@gmail.com>
2023-10-19 00:32:52 +0300

    Решение задачи 1

---
+++ Task_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static int second_chislo(int chislo){
+            int result = 0;
+            if (chislo >= 100 && chislo <= 999){
+                result = chislo / 10 % 10;
+            }
+            else{
+                Console.WriteLine("Введено неверное число. Введите число от 100 до 999");
+                chislo = Convert.ToInt32(Console.ReadLine());
+                if (chislo >= 100 && chislo <= 999){
+                result = chislo / 10 % 10;
+                }
+            }
+            return result; 
+        }
+
+       
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Введите число от 100 до 999");
+           int chislo = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine(second_chislo(chislo));
+        }
+    }
+}
